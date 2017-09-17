dfecd93724bbe2d3885f2187d31ea9e7d1304708
Aliciiak <30807185+Aliciiak@users.noreply.github.com>
2017-09-16 22:21:11 -0700

    Update application_controller.rb

--- application_controller.rb
+++ application_controller.rb
@@ -7,7 +7,28 @@ class MyApp < Sinatra::Base
     erb :index
   end
 
+  
+  post '/' do
+    erb :submission
+  end
+  
+  get '/submission.erb' do
+    # "Hello"
+    erb :submission
+  end
+  
+  post '/submission.erb' do
+    gift_choice = params[:type]
+    @giftidea = gifts(gift_choice)
+    @gift_array = @giftidea.split("++")
+    @title= @gift_array[0]
+    @image= @gift_array[1]
+    @description= @gift_array[2]
 
+    erb :results
+  end
+    
+    
 
 
 
